a90a2761dc61e57c23e4f585a0f5bb8bb16a47b6
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-04 08:40:22 +0100

    implement a new logic

--- carvan.py
+++ carvan.py
@@ -6,9 +6,18 @@ T = range(1, 100)
 N = range(1,10_000)
 
 t_input = int(input())
-if (t_input in T):
+if ((t_input in T) == True):
     for test in range(t_input):
         n_input = int(input())
+        answer = 1
         if (n_input in N):
-            n_speed = list(map(int, input().split()))
-            print(n_speed)
+            for n_speed in range(n_input):
+                n_speed = list(map(int, input().split())) # accept input in a line
+                for speed in range(len(n_speed)):
+                    if (n_speed[speed+1]>n_speed[speed]):
+                        n_speed[speed+1]=n_speed[speed]
+                    else:
+                        answer += answer
+                    print(n_input, answer)
+        else:
+            break
