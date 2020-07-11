1b81e9f264b130c8af2be82a6a2166a1932ac977
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-11 13:16:04 +0100

    remove; unnecessary line

--- carvan.py
+++ carvan.py
@@ -26,27 +26,3 @@ def get_n_speeds(test, numbers):
 
 test = int(input())
 numbers = input()
-get_n_speeds(test, numbers)
-# test_validitor(test)
-# cars_counter_validitor(numbers)
-
-# for testcases in range(test):
-#     n_speed_list = list(map(int, input().split()))
-
-# t_input = int(input())
-# if ((t_input in T) == True):
-#     for test in range(t_input):
-#         n_input = int(input())
-#         answer = 1
-#         if (n_input in N):
-#             for n_speed in range(n_input):
-#                 n_speed = list(map(int, input().split())) # accept input in a line
-                # for speed in range(len(n_speed)):
-                #     if (n_speed[speed+1]>n_speed[speed]):
-                #         n_speed[speed+1]=n_speed[speed]
-                #     else:
-                #         answer += answer
-                    # print(n_input, answer)
-        #         print(n_speed)
-        # else:
-        #     break
