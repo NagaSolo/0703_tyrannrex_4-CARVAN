3fc0a159e06725d5a812a4cd27f824e144e576bd
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-13 16:53:23 +0100

    carvantwo.py

--- carvantwo.py
+++ carvantwo.py
@@ -6,9 +6,5 @@ cars_w_max_speed = 1
 test = int(input())
 for t in range(test):
     number_of_cars = int(input())
-    for n in range(number_of_cars):
-        first_input = int(input())
-        next_input = int(input())
-        if next_input >= first_input:
-            cars_w_max_speed += 1
-    print(cars_w_max_speed)
+    dynamic_input = [int(car_speed) for car_speed in input().split() if range(number_of_cars)]
+    print(dynamic_input)
